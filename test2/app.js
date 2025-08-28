a6acdb24cf81b5a8ae59a908e9fb02388a7fa7b8
buksa656 <85954877+buksa656@users.noreply.github.com>
2025-08-28 14:38:26 +0200

    Update app.js

--- test2/app.js
+++ test2/app.js
@@ -1285,6 +1285,7 @@ class KorposzczurGame {
             const taskCard = this.createTaskCard(taskData, taskState);
             container.appendChild(taskCard);
         });
+        this.updateTaskButtonStates();
     }
 
     createTaskCard(taskData, taskState) {
